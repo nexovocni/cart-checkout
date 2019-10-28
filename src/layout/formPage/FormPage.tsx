05351cd0dc766830e2de81a55d2578c0d72a61b6
User <user@Users-MacBook-Pro.local>
2019-10-28 14:24:14 +0100

    layout-done

--- src/layout/formPage/FormPage.tsx
+++ src/layout/formPage/FormPage.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Email from './Email/Email'
 
 const FormPage = () => {
+
+const [component, openComponent] = useState(true)
+
     return (
-        <div>
-            <p>Hello</p>
+        <div className="form">
+            <Email component={component} openComponent={openComponent}/>
         </div>
     )
 }
