56580e777498d1f9dbb8e451fec8d144128cd609
User <user@Users-MacBook-Pro.local>
2019-12-11 17:25:07 +0100

    ready for rebase

--- src/components/SummarySubtotal/SummarySubtotal.tsx
+++ src/components/SummarySubtotal/SummarySubtotal.tsx
@@ -14,7 +14,7 @@ const SummarySubtotal:React.FC<IProps> = ({itemsValue, shipValue, disabledCode, 
     return (
         <div style={{opacity: disabledCode ? .3 : 1, pointerEvents: "none"}} className="summary__top__subtotal">
             <p>Subtotal</p>
-                <p className="summary__total__value">${((itemsValue + checkValue).toFixed(2))}</p>
+            <p className="summary__total__value">${((itemsValue + checkValue).toFixed(2))}</p>
         </div>
     )
 }
