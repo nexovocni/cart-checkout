daa557025517fab3c26d8bcdb0f2851c7f2f892b
User <user@Users-MacBook-Pro.local>
2020-01-17 14:03:25 +0100

    review refacturing

--- src/components/Review/Review.tsx
+++ src/components/Review/Review.tsx
@@ -1,70 +1,123 @@
-import React, {useState, useEffect, useContext} from 'react'
-import ReviewProduct from '../ReviewProduct/ReviewProduct'
-import ReviewData from '../ReviewData/ReviewData'
-import {FormComponentContext} from '../../contexts/FormComponentContext'
-import {ProductContext} from '../../contexts/ProductContext'
-import './Review.scss'
+import React, { useState, useEffect, useContext } from "react";
+import ReviewProduct from "../ReviewProduct/ReviewProduct";
+import ReviewData from "../ReviewData/ReviewData";
+import { FormComponentContext } from "../../contexts/FormComponentContext";
+import { ProductContext } from "../../contexts/ProductContext";
+import "./Review.scss";
 
-const Review:React.FC = () => {
+const Review: React.FC = () => {
+  const [stateComponent, updateStateComponent] = useState(false);
 
-    const [stateComponent, updateStateComponent] = useState (false)
+  const [scrollBtn, setScrollBtn] = useState(false);
 
-    const [scrollBtn, setScrollBtn] = useState(false)
+  const formContext: any = useContext(FormComponentContext);
+  const { formComponents, dispatch } = formContext;
+  const { componentReview, editReview } = formComponents;
 
-    const formContext:any = useContext(FormComponentContext)
-    const {formComponents, dispatch} = formContext
-    const {componentReview, editReview} = formComponents
+  const productContext: any = useContext(ProductContext);
+  const { products } = productContext;
 
-    const productContext:any = useContext(ProductContext)
-    const {products} = productContext
+  useEffect(() => {
+    document.addEventListener("scroll", () => {
+      const wrappedElement = document.getElementById("sticky_div");
 
-    useEffect(() => {
-        document.addEventListener("scroll", () => {
-            const wrappedElement = document.getElementById('sticky_div')
-              
-            if (Math.round(wrappedElement!.getBoundingClientRect().bottom) - window.innerHeight <= 0) {
-                setScrollBtn(true)
+      if (
+        Math.round(wrappedElement!.getBoundingClientRect().bottom) -
+          window.innerHeight <=
+        0
+      ) {
+        setScrollBtn(true);
+      }
+    });
+  }, [componentReview]);
+
+  const reviewSubmit = () => {
+    setScrollBtn(false);
+    dispatch({
+      type: "COMPONENT",
+      payload: { componentReview: false, editReview: true }
+    });
+  };
+
+  return (
+    <div className='review'>
+      <div className='review__up'>
+        <h2
+          style={{ color: !componentReview ? "#999" : "#000" }}
+          className='review__number'
+        >
+          4
+        </h2>
+        <div
+          style={{ backgroundColor: !componentReview ? "#999" : "#000" }}
+          className='dash-review'
+        ></div>
+        <h2
+          style={{ color: !componentReview ? "#999" : "#000" }}
+          className='review__heading'
+        >
+          Review and finish
+        </h2>
+        <button
+          style={{ opacity: !componentReview ? 1 : 0 }}
+          onClick={() => {
+            dispatch({
+              type: "COMPONENT",
+              payload: { componentReview: !componentReview }
+            });
+          }}
+          className={editReview ? "review__heading__button" : "editBtn"}
+        >
+          Edit
+        </button>
+      </div>
+      <div className='review__component'>
+        <div
+          className={
+            componentReview
+              ? "review__component__section"
+              : "review__component__section__close"
+          }
+        >
+          <div className='review__products'>
+            {products.map((product: any) => {
+              return (
+                <ReviewProduct
+                  product={product}
+                  stateComponent={stateComponent}
+                  updateStateComponent={updateStateComponent}
+                  key={product.id}
+                />
+              );
+            })}
+          </div>
+          <div className='review__data'>
+            <ReviewData />
+          </div>
+          <div
+            id='sticky_div'
+            className={
+              !scrollBtn
+                ? "review__component__button"
+                : "review__component__button-sticky"
             }
-        })
-    }, [componentReview])
-
-    const reviewSubmit = () => {
-        setScrollBtn(false)
-        dispatch({type: "COMPONENT", payload: {componentReview: false, editReview: true}})
-    }
-
-    return (
-        <div className="review">
-            <div className="review__up">
-            <h2 style={{color: !componentReview ? "#999" : "#000"}} className="review__number">4</h2>
-            <div style={{backgroundColor: !componentReview ? "#999" : "#000"}} className="dash-review"></div>
-            <h2 style={{color: !componentReview ? "#999" : "#000"}} className="review__heading">Review and finish</h2>
-            <button style={{opacity: !componentReview ? 1 : 0}} onClick={() => {dispatch({type: "COMPONENT", payload: {componentReview: !componentReview}})}} className={editReview ? "review__heading__button" : "editBtn"}>Edit</button>
-            </div>
-            <div className="review__component">
-                <div className={componentReview ? "review__component__section" : "review__component__section__close"}>
-                    <div className="review__products">
-                        {products.map((product:any) => {
-                            return(
-                                <ReviewProduct 
-                                    product={product}
-                                    stateComponent={stateComponent}
-                                    updateStateComponent={updateStateComponent}
-                                    key={product.id}
-                                />
-                            )
-                        })}
-                    </div>
-                    <div className="review__data">
-                        <ReviewData />
-                    </div>
-                    <div style={{position: stateComponent ? "relative" : "sticky"}} id="sticky_div" className={!scrollBtn ? "review__component__button" : "review__component__button-sticky"}>
-                        <button style={{opacity: stateComponent ? .3 : 1, pointerEvents: stateComponent ? "none" : "auto"}} onClick={reviewSubmit} className="review__component__submit" type="submit">Place order</button>
-                    </div>
-                </div>
-            </div>
+          >
+            <button
+              style={{
+                opacity: stateComponent ? 0.3 : 1,
+                pointerEvents: stateComponent ? "none" : "auto"
+              }}
+              onClick={reviewSubmit}
+              className='review__component__submit'
+              type='submit'
+            >
+              Place order
+            </button>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Review
+export default Review;
