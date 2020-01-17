939d7dd0b1ee9b7a7164f0efa6e9807583732857
User <user@Users-MacBook-Pro.local>
2020-01-17 17:15:08 +0100

    css refacturing

--- src/layout/cartPage/CartPage.tsx
+++ src/layout/cartPage/CartPage.tsx
@@ -1,27 +1,26 @@
-import React from 'react'
-import Header from './Header/Header'
-import Products from './Products/Products'
-import Form  from './Form/Form'
-import Summary from './Summary/Summary'
-import {ComponentContextProvider} from '../../contexts/FormComponentContext'
-import './CartPage.scss'
+import React from 'react';
+import Header from './Header/Header';
+import Products from './Products/Products';
+import Form from './Form/Form';
+import Summary from './Summary/Summary';
+import { ComponentContextProvider } from '../../contexts/FormComponentContext';
+import './CartPage.scss';
 
-const CartPage:React.FC = () => {
-
-    return (
-        <div className="cart__page">
-        <div className="cart__page__main">
-            <Header />
-            <div className="cart__page__main__content">
-            <Products />
-            <ComponentContextProvider>
-                <Form />
-            </ComponentContextProvider>
+const CartPage: React.FC = () => {
+  return (
+    <div className="cart__page">
+      <div className="cart__page__main">
+        <Header />
+        <div className="cart__page__main__content">
+          <Products />
+          <ComponentContextProvider>
+            <Form />
+          </ComponentContextProvider>
         </div>
-        </div>
-            <Summary />
+      </div>
+      <Summary />
     </div>
-    )
-}
+  );
+};
 
-export default CartPage
+export default CartPage;
